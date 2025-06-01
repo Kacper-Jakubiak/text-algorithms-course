a48c5aaec1d9307b805ea1dad5200d3abb9bf24c
langMonk <lang.monk93@gmail.com>
2025-06-01 19:12:27 +0200

    lab_6: sequence alignment mdx

---
+++ content/topics/06-sequence-alignment/_meta.js
@@ -0,0 +1,15 @@
+export default {
+    _: {
+        type: 'separator',
+        title: 'Wyrównywanie sekwencji i podobieństwo'
+    },
+    index: "Informacje",
+    __: {
+        type: 'separator',
+        title: 'Zadania'
+    },
+    '01-ex': 'Algorytm naiwny dla odległości edycyjnej',
+    '02-ex': 'Algorytm Wagnera-Fischera',
+    '03-ex': 'Algorytm Allisona',
+    '04-ex': 'Algorytm Nilsimsa',
+}
